597cec0e2f0ea553134b9ff81942b2e745786542
pedroz <pedroz@umich.edu>
2019-03-23 17:54:52 -0400

    bmp hopefully done

--- libraries/BMP/BMP.h
+++ libraries/BMP/BMP.h
@@ -9,26 +9,18 @@ class BMP{
 public:
     BMP();
     void begin();
-    void readAltitude();
-    void readUncompTemp();
-    void readUncompPress();
-    void printAllData();
+    float readAltitude();
+    float readTemperature();
+    float readPressure();
 private:
-    unsigned short dig_T1;
-    short dig_T2, dig_T3; 
-    unsigned short dig_P1;
-    short dig_P2, dig_P3, dig_P4, dig_P5, dig_P6, dig_P7, dig_P8, dig_P9;
-
-    signed long UT, UP;
+    uint16_t dig_T1;
+    int16_t dig_T2, dig_T3; 
+    uint16_t dig_P1;
+    int16_t dig_P2, dig_P3, dig_P4, dig_P5, dig_P6, dig_P7, dig_P8, dig_P9;
     
-    const int OverSampSett = 3; // set oversample rate
-    const int ConvTimeDel = 26; // set conversion time
-    const long p0 = 256.0;
+    const long p0 = 1025.0;
     
-    float T, P, A;
-    
-    long readToRegisters(int code, int numBytes);
-    void groupAddresses();
+    void readToRegisters();
 };
 
 #endif

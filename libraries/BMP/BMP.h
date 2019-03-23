32538cfa579366a757e919735a3b63690d2ada3c
pedroz <pedroz@umich.edu>
2019-03-23 17:00:32 -0400

    bmp library add

--- libraries/BMP/BMP.h
+++ libraries/BMP/BMP.h
@@ -14,11 +14,13 @@ public:
     void readUncompPress();
     void printAllData();
 private:
-    long AC1, AC2, AC3, H1, B2, MB, MC, MD;
-    unsigned long AC4, AC5, AC6; //unsigned vars from datasheet
+    unsigned short dig_T1;
+    short dig_T2, dig_T3; 
+    unsigned short dig_P1;
+    short dig_P2, dig_P3, dig_P4, dig_P5, dig_P6, dig_P7, dig_P8, dig_P9;
+
+    signed long UT, UP;
     
-    long X1, X2, X3, B3, B5, B6, UP, UT, p;
-    unsigned long B4, B7;
     const int OverSampSett = 3; // set oversample rate
     const int ConvTimeDel = 26; // set conversion time
     const long p0 = 256.0;

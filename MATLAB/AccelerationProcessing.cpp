dd84f25fd0ae6323f2427364409665221b984577
Arthur Zhang <arthur@azking-473.local>
2019-04-16 18:03:13 -0400

    post processed acc data

--- MATLAB/AccelerationProcessing.cpp
+++ MATLAB/AccelerationProcessing.cpp
@@ -1,9 +1,92 @@
-//
-//  AccelerationProcessing.cpp
-//  post_processing
-//
-//  Created by Arthur Zhang on 4/15/19.
-//  Copyright © 2019 ArthurKZhang. All rights reserved.
-//
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+#include <sstream>
 
-#include <stdio.h>
+using namespace std;
+
+const char DELIM = ',';
+
+bool isalnumeric(const string &input) {
+    for (int i = 0; i < input.size(); i++) {
+        if (!isalnum(input[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    // input txt file
+    ifstream fin("FLIGHT_DATA.TXT");
+    ofstream fout("processed_string.txt");
+    if (!fin.is_open()) {
+        cout << "Error opening file" << endl;
+    }
+    
+    vector< vector<string> > data;
+    string header;
+    getline(fin, header);
+    fout << "Time, " << "acc_x, " << "acc_y, " << "acc_y, " << "altitude, " << "MPU_x, " << " MPU_y, " << "MPU_z, " << endl;
+    
+    string currentline;
+    // parse strings
+    while(fin >> currentline) {
+        vector<string> temp;
+        if (currentline.find("GPGGA") != string::npos) {
+            string timeString = currentline;
+            stringstream time(timeString);
+            getline( time, timeString, DELIM );
+            if (timeString.find("GPGGA") == -1) {
+                temp.push_back(timeString + ",");
+                getline( time, timeString, DELIM );
+                temp.push_back(timeString + ", ");
+                getline( time, timeString, DELIM );
+                temp.push_back(timeString + ", ");
+                getline( time, timeString, DELIM );
+                temp.push_back(timeString + ", ");
+                getline( time, timeString, DELIM );
+                getline( time, timeString, DELIM );
+                temp.push_back(timeString + ", ");
+                // sets up for acc pos
+                for (int i = 0; i < 4; i++) {
+                    getline( time, timeString, DELIM );
+                }
+                
+                // save next three vals for mpu
+                temp.push_back(timeString + ", ");
+                getline( time, timeString, DELIM );
+                temp.push_back(timeString + ", ");
+                getline( time, timeString, DELIM );
+                temp.push_back(timeString + ", ");
+            }
+            
+//            currentline = currentline.substr(currentline.find("GPGGA")+5);
+//            stringstream ss(currentline);
+            
+//            vector<string> lineVector;
+//            while(getline( ss, currentline, DELIM )) {
+//                lineVector.push_back(currentline);
+//            }
+        }
+        if (temp.size() == 8) {
+            data.push_back(temp);
+        }
+        
+        //        pos++;
+    }
+    
+    // save all data to file
+    for (int i = 0; i < data.size(); i++) {
+        for(int j = 0; j < data[i].size(); j++) {
+            cout << data[i][j] << " ";
+            fout << data[i][j] << " ";
+        }
+        cout << endl;
+        fout << endl;
+    }
+    
+    fout.close();
+    return 0;
+}

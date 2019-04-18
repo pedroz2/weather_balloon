07f69bf3a7027d4bcfe9492b09158eadb7eff84f
Arthur Zhang <arthur@azking-473.local>
2019-04-18 15:56:48 -0400

    added acc values

--- MATLAB/post_process/acceleration_plots.m
+++ MATLAB/post_process/acceleration_plots.m
@@ -12,24 +12,22 @@ smooth_x = smooth(acc(300:7000,1), 500,'loess');
 smooth_y = smooth(acc(300:7000,2), 500,'loess');
 smooth_z = smooth(acc(300:7000,3), 500,'loess');
 
-% while (std(smooth_x) > 1 && std(smooth_y) > 1 && std(smooth_z) > 1) 
-%    smooth_x = sgolayfilt(smooth_x, 2, 19);
-%    smooth_y = sgolayfilt(smooth_y, 2, 19);
-%    smooth_z = sgolayfilt(smooth_z, 2, 19);
-% end
-
 acceleration = sqrt((smooth_x).^2 + (smooth_y).^2 + (smooth_z).^2);
 
+mean_acc = mean(acceleration);
+std_acc = std(acceleration);
+
+% Max acceleration at 1.0017 g at 13058 m
 figure()
 hold on
-line([0.82 1.02],[13900 13900],'LineWidth',1)
+line([0.82 1.02],[13852 13852],'LineWidth',1)
 plot(acceleration, altitude(300:7000), '-r');
 hold off
 xlim([0.82 1.02]);
 xlabel({"\fontsize{14}Acceleration (g)"}, 'FontSize', 14);
 ylabel("\fontsize{14}Altitude (m)", 'FontSize', 14);
-legend("\fontsize{12}Peak Accleration at 13900 m");
-title("\fontsize{16}Max Acceleration vs Altitude");
+legend("\fontsize{12}Peak Accleration at 13852 m");
+title("\fontsize{16}Magnitude of Acceleration vs Altitude");
 print("MaxAcceleration", "-dpng", "-r300");
 
 %% Smoothed Acceleration Minus Non Smoothed
@@ -57,21 +55,22 @@ hold on
 % plot(time, mag_smooth, '-b');
 
 % flight ascencsion
-ax1 = subplot(2,1,1);
-plot(difference_smooth(500:8000), altitude(500:8000), '-b', 'LineWidth',1);
+ax1 = subplot(1,2,1);
+subplot(1,2,1);
+plot(difference_smooth(500:8000), altitude(500:8000), '-r', 'LineWidth',1);
 xlim([-.25 .25]);
-title("\fontsize{16}Difference in Smoothed vs. Unsmoothed Acceleration vs. Altitude on Ascent");
+title("\fontsize{16}During Ascent");
 ylabel("\fontsize{14}Altitude (m)");
 xlabel("\fontsize{14}Acceleration Difference (g)");
 
-ax2 = subplot(2,1,1);
-subplot(2,1,2);
+ax2 = subplot(1,2,2);
+subplot(1,2,2);
 difference_smooth = smooth(difference_smooth, 200,'loess');
 plot(difference_smooth(8000:max_time), altitude(8000:max_time), '-r', 'LineWidth',2);
-line([-0.05 0.45],[18007 18007], 'LineWidth',1);
-line([-0.05 0.45],[10700 10700],'LineWidth',1);
+% line([-0.05 0.45],[18007 18007], 'LineWidth',1);
+% line([-0.05 0.45],[10700 10700],'LineWidth',1);
 xlim([-.05 .45]);
-title("\fontsize{16}Difference in Smoothed vs. Unsmoothed Acceleration vs. Altitude on Descent");
+title("\fontsize{16}During Descent")
 ylabel("\fontsize{14}Altitude (m)");
 xlabel("\fontsize{14}Acceleration Difference (g)");
 hold off
